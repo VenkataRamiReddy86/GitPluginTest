ce0b0bbb3ec1a2ed5c673da0150b76f3512ca893
thangiralav <venka@172.20.0.250>
2020-09-01 12:08:13 +0530

    commit to child branch

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -4,6 +4,10 @@ public class TestGit {
 
 	private void sysout() {
 		// TODO Auto-generated method stub
+		
+		public static void main(String[] args) { 
+		    System.out.println("Hello Git, from branch bug#1234 !!"); 
+		  } 
 
 	}
 }
